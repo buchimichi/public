25a1cda8f9f3d5823346842e0b0c5911931588d9
DESKTOP-MSM7NB5\02 <m.mitijet@gmail.com>
2017-12-12 17:08:01 +0900

    12/12編集分

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
 $(function () {
-
+    $(".nav-btn").click(function(){
+        $(".gnav").fadeToggle(500);  
+        $(".nav-btn").toggleClass("active");
+        return false;
+      });  
+    
 });
+
+                      
