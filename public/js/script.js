b59161045ffcf3f1d12edd4e868f02ffaecf32ad
DESKTOP-MSM7NB5\02 <m.mitijet@gmail.com>
2017-12-13 16:50:50 +0900

    12/13編集分

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,62 @@
 $(function () {
-    $(".nav-btn").click(function(){
-        $(".gnav").fadeToggle(500);  
-        $(".nav-btn").toggleClass("active");
+
+
+    //    ---------------gnavトグル--------------------
+    $(".menu").click(function () {
+        $(".menu1").toggleClass("menuclick1");
+        $(".gnav-sp").stop().fadeToggle(500);
+    });
+    $(".menu").click(function () {
+        $(".menu2").toggleClass("menuclick2");
+        $(".gnav-sp").stop().fadeToggle(500);
+    })
+    $(".menu").click(function () {
+        $(".menu3").toggleClass("menuclick3");
+        $(".gnav-sp").stop().fadeToggle(500);
+    });
+
+
+    //------------------aboutビューアー-------------------   
+    $(".about a").click(function () {
+        $("figure img").attr("src", $(this).attr("href"));
         return false;
-      });  
+    })
+
+
+
+
+    //    --------------------pagetop--------------
+    $(".page-top").click(function () {
+        $("html,body").animate({
+            scrollTop: "0"
+        }, 1000);
+        return false;
+    })
+    $gnavPos = $(".gnav-pc").offset().top;
+    $(window).scroll(function () {
+        $(window).scrollTop();
+        if ($(window).scrollTop() >= 500) {
+            $(".page-top").fadeIn();
+        } else {
+            $(".page-top").fadeOut();
+        }
+
+//    -----------------nav-sticky-------------
+        if ($gnavPos <= $(window).scrollTop()) {
+            $(".gnav-pc").addClass("sticky");
+        } else {
+            $(".gnav-pc").removeClass("sticky");
+        }
+    })
+//    -------------------スムーススクロール------
+    $(".gnav-pc a").click(function(){
+        var $this = $(this).attr("href");
+        
+        var target = $($this).offset().top;
+        $("html,body").animate({
+            scrollTop: target - 10
+        }, 1000);
+        return false;
+    })
     
 });
-
-                      
